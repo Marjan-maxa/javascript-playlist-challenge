bbff4e87c2dfd6971cc93077829374b4a8354860
Marjan-maxa <boktimd95@gmail.com>
2026-08-17 01:02:56 +0600

    lecture 3 and 4 completed

---
+++ lec3_loops&string.js
@@ -0,0 +1,70 @@
+for (var i = 1; i <= 5; i++) {
+  console.log("i:", i);
+}
+console.log(i);
+let i = 1;
+while (i <= 5) {
+  console.log("Marjan Boss");
+  i++;
+}
+let i = 1;
+do {
+  console.log("i=", i);
+  i++;
+} while (i < 3);
+let str = "ABCDEF";
+size = 0;
+for (let i of str) {
+  console.log("i=", i);
+  size++;
+}
+console.log("String size :", size);
+
+let students = {
+  Name: "Ashik",
+  Age: 21,
+  Address: "Nurpur",
+  mobile: "01954233387",
+};
+
+for (let key in students) {
+  console.log("keys: ", key, students[key]);
+}
+
+let i = 0;
+while (i <= 100) {
+  if (i % 2 !== 0) {
+    console.log("i :", i);
+  }
+  i++;
+}
+
+let gameNuumber = 75;
+let user = prompt("Enter your guess number :");
+while (gameNuumber != user) {
+  user = prompt("You lost the game,Please Again Enter your guess number: ");
+}
+console.log("You have won the game");
+
+let str = "Hello World!";
+
+let str2 = "Marjan";
+console.log(str2[2]); // r
+
+let info = {
+  name: "Marjan",
+  age: 22,
+};
+
+console.log(`${info.name} age is ${info.age} `);
+
+let str = "Hello\tWorld!"; // 12
+console.log(str.length);
+let str = "MarjanBoss";
+console.log(str.slice(1));
+
+let username = prompt("Enter your name :");
+atdaret = "@";
+let size = username.length;
+console.log(`Your Genared Username is:mash
+   ${atdaret.concat(username, size)}`);
--- lec4_array.js
+++ lec4_array.js
@@ -1,4 +1,48 @@
-let marks = [21, 43, 84, 12, 92];
-console.log(marks);
-let names = ["Marjan", "Raj", "Nur"];
-console.log(names);
+// string-- immutable
+// array -- mutable
+// let names = ["Marjan", "Raj", "Nur"];
+// console.log(names);
+// let marks = [21, 43, 84, 12, 92];
+// console.log(marks);
+// console.log(marks[0]);
+// marks[1] = 46;
+// console.log(marks);
+
+// let heroes = ["Baba", "Ammu", "Teacher", "Family"];
+// // //for loop
+// // for (let i = 0; i < heroes.length; i++) {
+// //   console.log(heroes[i]);
+// // }
+
+// // fot of loop use
+// for (let hero of heroes) {
+//   console.log(hero.toLowerCase());
+// }
+
+// average marks
+
+let studentMarks = [91, 81, 83, 78, 79, 85, 87];
+let sum = 0;
+for (let marks of studentMarks) {
+  sum += marks;
+}
+let avg = sum / studentMarks.length;
+console.log(`Average Maarks : ${avg.toFixed(2)}`);
+
+// 10% offer value print
+
+let items = [200, 300, 500, 600, 700];
+
+// for (let val of items) {
+//   let offer = val / items.length;
+//   items[i] = items[i] - offer;
+//   console.log(`Value After Offer :${items[i]}`);
+//   i++;
+// }
+for (let i = 0; i < items.length; i++) {
+  let offer = items[i] / items.length;
+  items[i] = items[i] - offer;
+}
+console.log(`Value After Offer :${items}\n`);
+
+//34:40 after start next day
