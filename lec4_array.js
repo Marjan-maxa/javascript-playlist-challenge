d107d3a3038c72679585d4f56735d5b0b78d2763
Marjan-maxa <boktimd95@gmail.com>
2026-08-18 19:54:23 +0600

    lec5 done

--- lec4_array.js
+++ lec4_array.js
@@ -1,93 +1,93 @@
-// // string-- immutable
-// // array -- mutable
-// let names = ["Marjan", "Raj", "Nur"];
-// console.log(names);
-// let marks = [21, 43, 84, 12, 92];
-// console.log(marks);
-// console.log(marks[0]);
-// marks[1] = 46;
-// console.log(marks);
+// string-- immutable
+// array -- mutable
+let names = ["Marjan", "Raj", "Nur"];
+console.log(names);
+let marks = [21, 43, 84, 12, 92];
+console.log(marks);
+console.log(marks[0]);
+marks[1] = 46;
+console.log(marks);
 
-// let heroes = ["Baba", "Ammu", "Teacher", "Family"];
-// // //for loop
-// // for (let i = 0; i < heroes.length; i++) {
-// //   console.log(heroes[i]);
-// // }
+let heroes = ["Baba", "Ammu", "Teacher", "Family"];
+//for loop
+for (let i = 0; i < heroes.length; i++) {
+  console.log(heroes[i]);
+}
 
-// // fot of loop use
-// for (let hero of heroes) {
-//   console.log(hero.toLowerCase());
-// }
+// fot of loop use
+for (let hero of heroes) {
+  console.log(hero.toLowerCase());
+}
 
-// // average marks
+// average marks
 
-// let studentMarks = [91, 81, 83, 78, 79, 85, 87];
-// let sum = 0;
-// for (let marks of studentMarks) {
-//   sum += marks;
-// }
-// let avg = sum / studentMarks.length;
-// console.log(`Average Maarks : ${avg.toFixed(2)}`);
+let studentMarks = [91, 81, 83, 78, 79, 85, 87];
+let sum = 0;
+for (let marks of studentMarks) {
+  sum += marks;
+}
+let avg = sum / studentMarks.length;
+console.log(`Average Maarks : ${avg.toFixed(2)}`);
 
-// // 10% offer value print
+// 10% offer value print
 
-// let items = [200, 300, 500, 600, 700];
-// let i = 0;
-// for (let val of items) {
-//   let offer = val / items.length;
-//   items[i] = items[i] - offer;
-//   console.log(`Value After Offer :${items[i]}`);
-//   i++;
-// }
-// for (let i = 0; i < items.length; i++) {
-//   let offer = items[i] / items.length;
-//   items[i] = items[i] - offer;
-// }
-// console.log(`Value After Offer :${items}\n`);
+let items = [200, 300, 500, 600, 700];
+let i = 0;
+for (let val of items) {
+  let offer = val / items.length;
+  items[i] = items[i] - offer;
+  console.log(`Value After Offer :${items[i]}`);
+  i++;
+}
+for (let i = 0; i < items.length; i++) {
+  let offer = items[i] / items.length;
+  items[i] = items[i] - offer;
+}
+console.log(`Value After Offer :${items}\n`);
 
-// //Array methods
-// let vegitables = ["tomato", "potato", "cucumber", "pepaya"];
-// let numbers = [75, 86, 76, 91, 85];
-// console.log(vegitables);
-// vegitables.push("mango");
-// console.log(vegitables);
-// let delated = vegitables.pop();
-// console.log("Delated vegitable :", delated);
+//Array methods
+let vegitables = ["tomato", "potato", "cucumber", "pepaya"];
+let numbers = [75, 86, 76, 91, 85];
+console.log(vegitables);
+vegitables.push("mango");
+console.log(vegitables);
+let delated = vegitables.pop();
+console.log("Delated vegitable :", delated);
 
-// let veg_str = vegitables.toString();
-// console.log(veg_str);
-// let num = numbers.toString();
-// console.log(num);
+let veg_str = vegitables.toString();
+console.log(veg_str);
+let num = numbers.toString();
+console.log(num);
 
-// //concut method
+//concut method
 
-// let marvelHeroes = ["Thor", "Ironman", "batman"];
-// let dcHeroes = ["Spiderman", "Shaktiman", "Padman", "Superman"];
-// let dhakaHeroes = ["Manna", "Shalman Shah", "Sakib Khan"];
-// let bestHeroes = marvelHeroes.concat(dcHeroes, dhakaHeroes);
+let marvelHeroes = ["Thor", "Ironman", "batman"];
+let dcHeroes = ["Spiderman", "Shaktiman", "Padman", "Superman"];
+let dhakaHeroes = ["Manna", "Shalman Shah", "Sakib Khan"];
+let bestHeroes = marvelHeroes.concat(dcHeroes, dhakaHeroes);
 
-// console.log(bestHeroes);
+console.log(bestHeroes);
 
-// //shif and unshift,slice,splice method works
+//shif and unshift,slice,splice method works
 
-// names = ["Marjan", "Nur", "Raj", "Nahin", "Abdullah"];
-// names.unshift("Antman");
-// console.log(names);
-// let val = names.shift();
-// console.log(`Deleted Data is :${val}`);
+names = ["Marjan", "Nur", "Raj", "Nahin", "Abdullah"];
+names.unshift("Antman");
+console.log(names);
+let val = names.shift();
+console.log(`Deleted Data is :${val}`);
 
-// console.log(`${names.slice()}`);
+console.log(`${names.slice()}`);
 
-// let er = names.splice(1, 2, "Mash", "Hidoy");
-// console.log(er);
-// ry = names.splice(0, 0, "Khan");
-// console.log(names);
+let er = names.splice(1, 2, "Mash", "Hidoy");
+console.log(er);
+ry = names.splice(0, 0, "Khan");
+console.log(names);
 
-// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
-// let deleted = companies.shift();
-// console.log(`Deleted Data : ${deleted}`);
-// console.log(companies);
-// companies.splice(1, 1, "Ola");
-// console.log(companies);
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+let deleted = companies.shift();
+console.log(`Deleted Data : ${deleted}`);
+console.log(companies);
+companies.splice(1, 1, "Ola");
+console.log(companies);
 
-// let valu = companies.push("Amazon");
+let valu = companies.push("Amazon");
--- lec5_function&metods.js
+++ lec5_function&metods.js
@@ -7,4 +7,126 @@ function display(msg) {
 
 display("I learn js Everyday"); // this is called arguments
 
-// 13:37 after start in the morning
+function sum(a, b, c) {
+  console.log(`The Sum of three numbers is : ${a + b + c}`);
+}
+sum(10, 20, 30);
+
+function day(x, y) {
+  // scope define ; x scope of body, like local variables of function ;  block scope
+  console.log(x);
+  s = x + y;
+
+  return s;
+}
+
+let val = day(10, 3);
+console.log(val);
+
+let g = 75;
+
+// it is arrow function. This is modern js features
+const multiply = (e, f) => {
+  return e * f;
+};
+
+// count noumber of  vowel
+
+function vowelCount(str) {
+  let count = 0;
+  for (const vall of str.toLowerCase()) {
+    if (
+      vall === "a" ||
+      vall === "e" ||
+      vall === "i" ||
+      vall === "o" ||
+      vall === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+// same task , but using arrow function
+const vowCount = (str) => {
+  let count = 0;
+  for (const vall of str.toLowerCase()) {
+    if (
+      vall === "a" ||
+      vall === "e" ||
+      vall === "i" ||
+      vall === "o" ||
+      vall === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log(count);
+};
+
+// methods ,  forEach loop
+// **** forEach loop is akinnd of higher order method / higher order method
+//   ||
+//  function as a parameter
+// functin as a return
+let arr = ["Marjan", "Nur", "Rakib", "Raj", "Abdullah"];
+console.log("The Values of Arrow are :");
+arr.forEach((va, index, array) => {
+  console.log(va.toUpperCase(), index, array);
+});
+
+// numbers of square of array
+//..................................
+
+numbers = [5, 7, 9];
+numbers.forEach((numm) => {
+  console.log(numm * numm);
+});
+
+// same work
+numbers = [5, 7, 9];
+let results = (numm) => {
+  console.log(numm * numm);
+};
+numbers.forEach(results);
+
+// ever number filter
+
+let allNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let evenNumbers = allNumbers.filter((even) => {
+  return even % 2 === 0;
+});
+console.log(evenNumbers);
+
+//reduce method
+let digit = [1, 2, 3, 4, 5];
+let output = digit.reduce((pre, current) => {
+  return pre > current ? pre : current;
+});
+console.log(output);
+
+//filter greter than  90
+
+let num1 = [75, 82, 93, 67, 90, 91];
+let newArray = num1.filter((va) => {
+  return va > 90;
+});
+console.log(newArray);
+
+let n = prompt("Enter a number : ");
+let array1 = [];
+for (let i = 1; i <= n; i++) {
+  array1[i - 1] = i;
+}
+console.log(array1);
+
+let calculate = array1.reduce((pre, curr) => {
+  return pre + curr;
+});
+console.log("Sum is  : ", calculate);
+
+let calculate2 = array1.reduce((pre, curr) => {
+  return pre * curr;
+});
+console.log("Product/factorial is  : ", calculate2);
