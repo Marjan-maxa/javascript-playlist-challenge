accfb531fa4dbff6a2597c83dcd0da8c97f1f67c
Marjan-maxa <boktimd95@gmail.com>
2026-08-20 01:35:27 +0600

    lec6 learning

---
+++ lec6_dom.js
@@ -0,0 +1,13 @@
+// alert("Hello Worlds!");
+// window.console.log("Marjan");
+
+// let Heading = document.getElementById("heading"); // h1
+// console.dir(Heading);
+// let Heading2 = document.getElementsByClassName("heading2-class");
+// console.dir(Heading2);
+// let parags = document.getElementsByTagName("p");
+// console.dir(parags);
+
+let firstEl = document.querySelectorAll("p");
+console.dir(firstEl);
+//52:18
